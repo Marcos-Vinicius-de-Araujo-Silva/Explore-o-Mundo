b908d7b5e571797ba94434436bcdf94a113f14bb
Marcos <araujomarcos60@gmail.com>
2026-01-24 21:45:44 -0300

    first commit

---
+++ html_course/javascript.js
@@ -0,0 +1,4 @@
+const num1 = prompt("Digite um número:");
+const num2 = prompt("Digite outro número:");
+
+alert("Média: " + ( ( parseFloat(num1) + parseFloat(num2) ) / 2 ) );
---
+++ html_course/script.js
@@ -0,0 +1,18 @@
+let typeValue = null
+
+switch (typeValue) {
+    case true:
+        console.log("The value is true");
+        break;
+    case false:
+        console.log("The value is false");
+        break;
+    case null:
+        console.log("The value is null");
+        break;
+    case undefined:
+        console.log("The value is undefined");
+        break;
+    default:
+        console.log("The value is of an unexpected type");
+}
