b514592dbd6430594766d1d467a429c6bcf04dd5
Marcos <araujomarcos60@gmail.com>
2026-03-09 21:58:40 -0300

    termino do main e do footer

--- html_course/script.js
+++ html_course/script.js
@@ -1,13 +1,5 @@
-const endereço = {
-		rua: "Rua das Tulias",
-		numero: 302
-	}
-
 const pessoa = {
-	nome: "João",
-	sobrenome: "Fagundes",
-    endereço: endereço
-};
+	nome: String,
+	email: String
+}
 
-
-document.write(`O endereço é ${pessoa.endereço.rua}, número ${pessoa.endereço.numero}.<br>`);
